488b4c92fa571ee3d33e40a99154b64ee41476a1
MinhNang-gif <leminhnang2k7@gmail.com>
2024-11-18 10:02:26 +0700

    Minh Năng thêm điều kiện else trong hàm Update

--- Snake.cpp
+++ Snake.cpp
@@ -14,6 +14,10 @@ void Snake::Update() {
     if (current_cell.x != prev_cell.x || current_cell.y != prev_cell.y) {
         UpdateBody(current_cell, prev_cell);
     }
+    // Thêm điều kiện tối ưu hóa, chỉ cập nhật thân khi rắn di chuyển
+    else {
+        std::cout << "Snake did not move, skipping body update.\n";
+    }
 }
 void Snake::UpdateHead() {
     switch (direction) {
