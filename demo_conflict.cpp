1fe46ad3f5bf45cb9739ba7b1797364e0f69b259
MinhNang-gif <leminhnang2k7@gmail.com>
2024-11-19 19:59:20 +0700

    MN add world9

--- demo_conflict.cpp
+++ demo_conflict.cpp
@@ -10,11 +10,9 @@ int main() {
     cout << "world4" << endl;
     cout << "world6" << endl;
     cout << "world5" << endl;
-<<<<<<< HEAD
     cout << "world8" << endl;
-=======
     cout << "world7" << endl;
->>>>>>> 5e523065bfd0299ae3feebe667364410933a5939
+    cout << "world9" << endl;
     return 0;
 }
 
